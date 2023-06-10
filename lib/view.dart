99ca7b4be2bd698c6b9b6acf8ebbb88020b8cb4c
Shayri <viranipradip100@gmail.com>
2023-06-10 08:00:27 +0530

    Initial commit

---
+++ lib/view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class View extends StatefulWidget {
+ int number=0;
+
+  @override
+  State<View> createState() => _ViewState();
+}
+
+class _ViewState extends State<View> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
